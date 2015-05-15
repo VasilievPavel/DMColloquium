a0c7fdd707c5150fa5dbf8b88dd0ca2d45006e62
VasilievPavel <xorus111@gmail.com>
2015-05-15 21:38:20 +0300

    Polynomial.h

---
+++ Polynomial.h
@@ -0,0 +1,22 @@
+//
+//  Polynomial.h
+//  DMColloquium
+//
+//  Created by Ilya Kulebyakin on 4/28/15.
+//  Copyright (c) 2015 Ilya Kulebyakin. All rights reserved.
+//
+
+#ifndef __DMColloquium__Polynomial__
+#define __DMColloquium__Polynomial__
+
+#include "RationalNumber.h"
+
+#endif /* defined(__DMColloquium__Polynomial__) */
+
+class Polynomial {
+private:
+    std::vector<RationalNumber> coefficients;
+public:
+    RationalNumber MUL_P_Q(RationalNumber &obj1);
+    
+};
