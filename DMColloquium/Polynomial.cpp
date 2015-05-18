4b926199653a1f2f90d8eb997f5b1ef0cc6f4f45
VasilievPavel <xorus111@gmail.com>
2015-05-18 21:11:16 +0300

    Васильев П.
    add MUL_P_Q

--- DMColloquium/Polynomial.cpp
+++ DMColloquium/Polynomial.cpp
@@ -7,3 +7,10 @@
 //
 
 #include "Polynomial.h"
+RationalNumber Polynomial::MUL_P_Q(RationalNumber &obj1)
+{
+    RationalNumber result;
+    result=this->MULL_ZZ_Z(obj1);
+    return result;
+    
+}
